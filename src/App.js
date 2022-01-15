2c69951a591912c68f5fbf62fffa96519a5e6e82
mehediemon007 <mehediemon7@gmail.com>
2022-01-15 22:39:03 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Routes , Route} from "react-router-dom";
+import Header from "./container/Header"
+import ProductListing from './container/ProductListing';
+import ProductDetails from './container/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      <h1>Boss Is Back!!!!</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing/>}/>
+          <Route path="/product/:productId" exact element={<ProductDetails/>}/>
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/container/ProductComponent.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react'
+import {Link} from "react-router-dom"
+import {useSelector} from "react-redux";
+
+function ProductComponent() {
+    const products = useSelector(state => state.allProducts.products)
+    const renderList = products.map((product) => {
+        const { id, title, image , price , category} = product;
+        return(
+            <div className='four wide column' key={id}>
+                <Link to={`/product/${id}`}>
+                    <div className='ui link cards'>
+                        <div className='card'>
+                            <div className='ui image'>
+                                <img src={image} alt={title} />
+                            </div>
+                            <div className='content'>
+                                <div className='header'>{title}</div>
+                                <div className='meat-price'>${price}</div>
+                                <div className='meta'>{category}</div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        )
+    });
+
+    return <>{renderList}</>
+}
+
+export default ProductComponent;
